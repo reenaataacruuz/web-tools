cb9e95444c5f0e3f239be53191ef593c686c51ca
Renata Cruz <renata.cruz@cobasi.com.br>
2024-04-01 23:16:56 -0300

    refac: ec6

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,19 @@
-function addTask() {
-    var taskInput = document.getElementById("taskInput");
-    var taskList = document.getElementById("taskList");
-    var taskText = taskInput.value.trim();
+const addTask = () => {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+    const taskText = taskInput.value.trim();
   
     if (taskText !== "") {
-      var taskItem = document.createElement("li");
+      const taskItem = document.createElement("li");
       taskItem.textContent = taskText;
+      
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "🗑️";
+      deleteButton.onclick = () => {
+        taskList.removeChild(taskItem);
+      };
+  
+      taskItem.appendChild(deleteButton);
       taskList.appendChild(taskItem);
       taskInput.value = "";
     }
